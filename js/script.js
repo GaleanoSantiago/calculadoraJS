faa6c6358b8cf132a4d433b0ed1438dd2fec10bb
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2023-09-07 23:06:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const btnsAppend = document.querySelectorAll(".btn-append");
+const btnClear = document.getElementById("btn-clear");
+const btnCalculate = document.getElementById("btn-calculate");
+const btnDelete = document.getElementById("btn-delete");
+
+let displayValue = '';
+
+const clearDisplay = ()=> {
+    displayValue = '';
+    updateDisplay();
+}
+
+const deleteCaracter = ()=>{
+    displayValue = displayValue.slice(0, -1); // Elimina el último carácter
+    updateDisplay();
+
+}
+
+const appendToDisplay = (value) => {
+    displayValue += value;
+    updateDisplay();
+}
+
+const calculate = () => {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+const updateDisplay = () => {
+    document.getElementById('display').value = displayValue;
+}
+
+btnsAppend.forEach(btn =>{
+    btn.addEventListener("click",()=>{
+        appendToDisplay(btn.textContent);
+    })
+})
+
+btnDelete.addEventListener("click", deleteCaracter);
+
+btnCalculate.addEventListener("click", calculate);
+
+btnClear.addEventListener("click", clearDisplay);
